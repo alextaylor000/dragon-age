50a69ad8fc87322bd52b222418a9e8918b32b4a5
Alex Taylor <alexmctaylorpants@gmail.com>
2016-05-21 00:35:09 -0700

    Add a button that disappears

--- nwb.config.js
+++ nwb.config.js
@@ -2,6 +2,13 @@ module.exports = {
   type: 'react-app',
   webpack: {
     extra: {
+      resolve: {
+        modulesDirectories: [
+          `node_modules`,
+          `src`
+        ],
+        extensions: [``,`,js`]
+      },
       externals: {
         'react/addons': true,
         'react/lib/ExecutionEnvironment': true,
--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import {Button} from 'components/Button'
 
-export default React.createClass({
-  render() {
-    return <div>
-      <h2>Welcome to Dragon Age</h2>
-    </div>
+export class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {showPlayButton: true}
+
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+    this.renderButton = this.renderButton.bind(this)
   }
-})
+
+  handleButtonClick(e) {
+    console.log('handle button click')
+    console.log(this)
+    this.setState({showPlayButton: false})
+  }
+
+  hello(e) {
+    console.log('helo')
+  }
+
+  renderButton() {
+    debugger
+    if (this.state.showPlayButton) {
+      return (<Button onClick={() => { console.log('hi') }} text="Play" />)
+    }
+  }
+
+  render() {
+    const button = this.state.showPlayButton ? <Button handleClick={this.handleButtonClick} text="hey" /> : ''
+    return (
+      <div>
+        <h2 onClick={this.hello.bind(this)}>Welcome to Dragon Age</h2>
+        {button}
+      </div>
+    )
+  }
+}
