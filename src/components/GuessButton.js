7e53b5676b4aafc3f76e039a670f0e053f968ea1
Alex Taylor <alexmctaylorpants@gmail.com>
2016-05-23 16:16:24 -0700

    Make it work on those cellular telephones

--- src/components/GuessButton.js
+++ src/components/GuessButton.js
@@ -5,6 +5,7 @@ export const GuessButton = ({age, onClick}) => {
     key={age}
     onClick={onClick}
     style={{margin: '25px', backgroundColor: 'yellow', border: '2px solid white', padding: '30px', cursor: 'pointer'}}
+    className='guess'
     value={age}>
       <a href='#'>
         {age.toLocaleString()} years old
