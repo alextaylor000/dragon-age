b5eaee886b2e9223b02ad8a46e1668158fb7d2ce
Alex Taylor <alexmctaylorpants@gmail.com>
2016-05-23 17:05:46 -0700

    Load guesses after image load

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,15 +6,23 @@ import {randomAge} from 'utils/randomAge'
 export class Card extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { imageLoaded: false }
     this.verifyGuess = this.verifyGuess.bind(this)
+    this.renderGuesses = this.renderGuesses.bind(this)
   }
 
   verifyGuess(guess) {
     this.props.onGuess(guess.target.value == this.props.dragon.age)
   }
 
-  render() {
+  renderGuesses() {
     const guesses = shuffle([this.props.dragon.age, randomAge(), randomAge()])
+    if (this.state.imageLoaded) {
+      return guesses.map((age) => { return <GuessButton age={age} key={age} onClick={this.verifyGuess} />})
+    }
+  }
+
+  render() {
     const imgContainerStyle = {
       border: '3px dashed red',
       minHeight: '325px'
@@ -23,8 +31,10 @@ export class Card extends React.Component {
     return (
       <div class="dragon-card">
         <h1><img src="/images/bigfire.gif" width="75"/>GUESS TEH DRAGOn AGE<img src="/images/bigfire.gif" width="75"/></h1>
-        <div style={imgContainerStyle}><img src={`/images/${this.props.dragon.image}`} /></div>
-        {guesses.map((age) => { return <GuessButton age={age} key={age} onClick={this.verifyGuess} />})}
+        <div style={imgContainerStyle}>
+          <img onLoad={() => this.setState({ imageLoaded: true})} src={`/images/${this.props.dragon.image}`} />
+        </div>
+        {this.renderGuesses()}
       </div>
     )
   }
